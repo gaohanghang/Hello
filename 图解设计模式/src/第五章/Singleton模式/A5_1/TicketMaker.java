aa29ce73bc6151cb7e1ef1e2128418ed41efa68d
gaohanghang <1341947277@qq.com>
2018-03-22 18:01:20 +0800

    单例模式

---
+++ 图解设计模式/src/第五章/Singleton模式/A5_1/TicketMaker.java
@@ -0,0 +1,19 @@
+package 第五章.Singleton模式.A5_1;
+
+
+public class TicketMaker {
+    private int ticket = 1000;
+
+    private static TicketMaker singleton = new TicketMaker();
+
+    private TicketMaker() {
+    }
+
+    public static TicketMaker getInstance() {
+        return                            singleton;
+    }
+
+    public synchronized int getNextTicketNumber() {
+        return ticket++;
+    }
+}
---
+++ 图解设计模式/src/第五章/Singleton模式/A5_2/Triple.java
@@ -0,0 +1,26 @@
+package 第五章.Singleton模式.A5_2;
+
+public class Triple {
+    private static Triple[] triple = new Triple[]{
+            new Triple(0),
+            new Triple(1),
+            new Triple(2),
+    };
+    private int id;
+
+    private Triple(int id) {
+        System.out.println("The instance " + id + " is created.");
+        this.id = id;
+    }
+
+    public static Triple getInstance(int id) {
+        return triple[id];
+    }
+
+    @Override
+    public String toString() {
+        return "Triple{" +
+                "id=" + id +
+                '}';
+    }
+}
